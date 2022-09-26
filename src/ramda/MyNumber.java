7dc6a1f1a67aae039ee5b3a8a11f937dc8d7c7ad
Geondol <keedob@naver.com>
2022-09-26 16:19:25 +0900

    람다

---
+++ src/ramda/MyNumber.java
@@ -0,0 +1,6 @@
+package ramda;
+
+public interface MyNumber {
+
+    int getMaxNumber(int num1, int num2);
+}
---
+++ src/ramda/Ramda.java
@@ -0,0 +1,11 @@
+package ramda;
+
+public class Ramda{
+    public static void main(String[] args) {
+
+        MyNumber maxNum = (x, y) -> (x >= y) ? x:y;
+        int max = maxNum.getMaxNumber(10,20);
+        System.out.println("max = " + max);
+
+    }
+}
